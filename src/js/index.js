06325406d6904a9953b65bd0fed4ef73d2198fc5
細田敦史 <hosodaaaaa>
2021-02-01 00:15:17 +0900

    🔨  スライダー作成

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,14 @@
-import '../stylus/index.styl'
+const sliderInit = () => {
+  new Swiper('.swiper-container', {
+    loop: true,
+    effect: 'fade',
+    slidesPerView: 1,
+    speed: 3000,
+    autoplay: {
+      delay: 3000,
+      DisableOnInteraction: false,
+    },
+  })
+}
 
-window.addEventListener('DOMContentLoaded', () => console.log('hy bigG'))
+window.addEventListener('DOMContentLoaded', sliderInit)
